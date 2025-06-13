170b66a45de3f4f8a121c3ab3e68674f24926224
silenceyeww <fbrchoppa@gmail.com>
2025-06-12 21:11:38 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,12 @@
 -- Converted with Script Source Converter
+-- Generated on 2025-06-13T01:11:34.949Z
+
+
+-- Obfuscated with Script Source Converter
+local _vBFLnUZq = function() return "MC43NTQ0MTYxNjY4NDY2Nzk5" end
+local _ekoeRlvK = {0.5744079242202961,0.38993008313343125,0.6255113824643176,0.4381527962413655,0.8452755208236997}
+if _zlPASchC ~= nil then _njObOvft = nil end
+-- Converted with Script Source Converter
 -- Generated on 2025-06-13T01:09:37.304Z
 
  - - Converted with Script Source Converter - - Generated on 2025 - 06 - 13T01:09:31.120Z loadstring(game:HttpGet("https: / / raw.githubusercontent.com / SpaceScriptHUB / petspawner / refs / heads / main / GrowaGardenVisual", true))()
